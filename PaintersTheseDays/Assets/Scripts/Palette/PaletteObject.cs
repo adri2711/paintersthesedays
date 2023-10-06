73f5c937b9d7fc3bdb221d62ea8c6851fa35fc3b
adri2711 <adri271101@gmail.com>
2023-10-06 03:10:09 +0200

    Squashed commit of the following:
    commit 3bda79f7e021b4680c5da2ecc88943450610bfd8
    Author: adri2711 <adri271101@gmail.com>
    Date:   Fri Oct 6 03:09:59 2023 +0200
    
        cleanup
    
    commit 34277b06eb1d987eb4ec844b25cec190ad936bca
    Merge: 12f2164 f2f58a3
    Author: adri2711 <adri271101@gmail.com>
    Date:   Fri Oct 6 03:09:01 2023 +0200
    
        Merge branch 'activate-palette' of https://github.com/adri2711/paintersthesedays into activate-palette
    
    commit 12f2164872f45e709d86bfeb3db66179112aae32
    Author: adri2711 <adri271101@gmail.com>
    Date:   Fri Oct 6 03:08:54 2023 +0200
    
        Paint Mixing
    
    commit f2f58a3a5dac6fee4b174ca5f51be88a04538568
    Author: abel pujol <abelpujol99@gmail.com>
    Date:   Thu Oct 5 22:53:05 2023 +0200
    
        Brush
    
    commit 970858d3955d7632416b1a3fd7e3ce7c07d793e2
    Author: Adri <adri271101@gmail.com>
    Date:   Thu Oct 5 13:52:46 2023 +0200
    
        Show/Hide palette animations
    
    commit 02c587a203718367f34b9d167d072a06d0725586
    Author: abel pujol <abelpujol99@gmail.com>
    Date:   Thu Oct 5 12:42:37 2023 +0200
    
        Shit

---
+++ PaintersTheseDays/Assets/Scripts/Palette/PaletteObject.cs
@@ -0,0 +1,173 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PaletteObject : MonoBehaviour
+{
+    private GameObject _characterSignalsInterfaceTarget;
+    private ICharacterSignals _characterSignals;
+    private FirstPersonController _firstPersonController;
+    private PaintingController _paintingController;
+    private Camera _camera;
+    private MeshRenderer _model;
+    private Object _paintChunkPrefab;
+    private List<Paint> _paints = new List<Paint>();
+    private List<PaintChunk> _paintChunks = new List<PaintChunk>();
+    private PaintChunk _mixerPaintChunk;
+    private bool _canClick = true;
+    bool _active = false;
+
+    void Start()
+    {
+        _paintChunkPrefab = Resources.Load("Prefab/Models/PaintChunk");
+        _characterSignalsInterfaceTarget = transform.parent.parent.parent.gameObject;
+        _characterSignals = _characterSignalsInterfaceTarget.GetComponent<ICharacterSignals>();
+        _firstPersonController = _characterSignalsInterfaceTarget.GetComponent<FirstPersonController>();
+        _paintingController = GetComponentInParent<PaintingController>();
+        _camera = GetComponentInParent<Camera>();
+        _model = transform.Find("PaletteModel").GetComponent<MeshRenderer>();
+        _model.enabled = false;
+
+        _characterSignals.PlacedCanvas.Subscribe(w =>
+        {
+            Activate();
+        }).AddTo(this);
+        _characterSignals.ExitedCanvas.Subscribe(w =>
+        {
+            Deactivate();
+        }).AddTo(this);
+    }
+
+    void Update()
+    {
+        if (_canClick && _firstPersonController.currentActiveCanvas != null)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                ClickChunk();
+            }
+            if (Input.GetMouseButton(1))
+            {
+                ClickChunk(true);
+            }
+        }
+    }
+    private void ClickChunk(bool right = false)
+    {
+        RaycastHit hit;
+        if (!Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out hit)) return;
+        PaintChunk paintChunk = hit.collider.GetComponent<PaintChunk>();
+        if (paintChunk == null) return;
+        if (right) 
+        {
+            if (paintChunk.isMix)
+            {
+                ConfirmMix();
+                GeneratePaintChunks();
+            }
+            else if (_mixerPaintChunk == null)
+            {
+                Object c = Instantiate(_paintChunkPrefab, _model.transform);
+                _mixerPaintChunk = c.AddComponent<PaintChunk>();
+                _mixerPaintChunk.Setup(paintChunk.paint, true);
+                _paintChunks.Add(_mixerPaintChunk);
+                _mixerPaintChunk.timesMixed++;
+            }
+            else
+            {
+                _mixerPaintChunk.timesMixed++;
+                AddPaintToMix(paintChunk.paint, Mathf.Max(1f / _mixerPaintChunk.timesMixed, 0.15f));
+                UpdatePaintMixMaterial();
+            }
+        }
+        else
+        {
+            _paintingController.SetSelectedBrushPaint(paintChunk.paint);
+        }
+        StartCoroutine(ClickDelay(0.2f));
+    }
+    public void AddPaintToMix(Paint p, float w)
+    {
+        if (_mixerPaintChunk.paint == null)
+        {
+            _mixerPaintChunk.paint = p;
+        }
+        else
+        {
+            _mixerPaintChunk.paint = Paint.CombinePaint(p, _mixerPaintChunk.paint, w);
+        }
+    }
+    public void ConfirmMix()
+    {
+        _paints.Add(_mixerPaintChunk.paint);
+    }
+    public void ClearMix()
+    {
+        Destroy(_mixerPaintChunk);
+    }
+    public void AddPaint(Color c)
+    {
+        _paints.Add(new Paint(c));
+    }
+    public void SetPaints(Color[] colors)
+    {
+        _paints = new List<Paint>();
+        foreach (Color c in colors)
+        {
+            _paints.Add(new Paint(c));
+        }
+    }
+    private void Activate()
+    {
+        _active = true;
+        SetPaints(new Color[] { Color.cyan, Color.yellow, Color.magenta, Color.black});
+        _model.enabled = true;
+        _model.GetComponent<Animator>().Play("Show");
+        GeneratePaintChunks();
+    }
+    private void Deactivate()
+    {
+        _active = false;
+        StartCoroutine(DeactivateCoroutine());
+    }
+    private IEnumerator DeactivateCoroutine()
+    {
+        _model.GetComponent<Animator>().Play("Hide");
+        yield return new WaitForSeconds(1f);
+        _model.enabled = _active;
+    }
+    private IEnumerator ClickDelay(float t)
+    {
+        _canClick = false;
+        yield return new WaitForSeconds(t);
+        _canClick = true;
+    }
+    private void UpdatePaintMixMaterial()
+    {
+        _mixerPaintChunk.UpdateMaterial();
+    }
+    private void GeneratePaintChunks()
+    {
+        foreach (PaintChunk p in _paintChunks)
+        {
+            Destroy(p.gameObject);
+        }
+        _paintChunks.Clear();
+        for (int i = 0; i < _paints.Count; i++)
+        {
+            Object c = Instantiate(_paintChunkPrefab, _model.transform);
+            PaintChunk paintChunk = c.AddComponent<PaintChunk>();
+            paintChunk.Setup(_paints[i]);
+            float a = Mathf.Deg2Rad * (90f + Mathf.Pow(-1, i + 1) * Mathf.Floor((i + 1) / 2) * 2f * 140f / Mathf.Max(_paints.Count, 5));
+            Vector3 r = new Vector3(Mathf.Sin(a), Mathf.Cos(a), 0f);
+            float o = 0.03f;
+            Vector3 pos = paintChunk.transform.localPosition + r * o;
+            paintChunk.transform.localPosition = pos;
+            _paintChunks.Add(paintChunk);
+        }
+    }
+}
--- PaintersTheseDays/Assets/Scripts/PlayerController/Painting/PaintingController.cs
+++ PaintersTheseDays/Assets/Scripts/PlayerController/Painting/PaintingController.cs
@@ -10,9 +10,9 @@ public class PaintingController : MonoBehaviour
     private GameObject _characterSignalsInterfaceTarget;
     private FirstPersonController _firstPersonController;
     private Camera _camera;
-    List<Brush> brushes = new List<Brush>();
+    List<Brush> _brushes = new List<Brush>();
     int _selectedBrush = 0;
-    bool canScroll = true;
+    bool _canScroll = true;
 
     void Start()
     {
@@ -21,9 +21,9 @@ public class PaintingController : MonoBehaviour
         _firstPersonController = _characterSignalsInterfaceTarget.GetComponent<FirstPersonController>();
         _camera = GetComponent<Camera>();
 
-        brushes.Add(new Brush(Color.cyan, 3, 4f));
-        brushes.Add(new Brush(Color.yellow, 5, 4f));
-        brushes.Add(new Brush(Paint.CombineColors(Color.red, Color.blue, 0.7f), 3, 2f));
+        _brushes.Add(new Brush(Color.cyan, 3, 4f));
+        _brushes.Add(new Brush(Color.yellow, 5, 4f));
+        _brushes.Add(new Brush(Paint.CombineColors(Color.red, Color.blue, 0.7f), 3, 2f));
 
     }
 
@@ -41,24 +41,29 @@ public class PaintingController : MonoBehaviour
             }
         }
 
-        if (canScroll && Mathf.Abs(Input.mouseScrollDelta.y) >= 1)
+        if (_canScroll && Mathf.Abs(Input.mouseScrollDelta.y) >= 1)
         {
-            _selectedBrush = (_selectedBrush + (int)Input.mouseScrollDelta.y + brushes.Count) % brushes.Count;
+            _selectedBrush = (_selectedBrush + (int)Input.mouseScrollDelta.y + _brushes.Count) % _brushes.Count;
             StartCoroutine(BrushScrollDelay(0.2f));
         }
     }
 
+    public void SetSelectedBrushPaint(Paint p)
+    {
+        _brushes[_selectedBrush].paint = p;
+    }
+
     private IEnumerator BrushScrollDelay(float t)
     {
-        canScroll = false;
+        _canScroll = false;
         yield return new WaitForSeconds(t);
-        canScroll = true;
+        _canScroll = true;
     }
 
     private void PaintWithBrush(bool rightClick = false)
     {
-        int iterations = rightClick ? 1 : brushes[_selectedBrush].density;
-        float sizeIncrease = rightClick ? 1f : brushes[_selectedBrush].dispersion;
+        int iterations = rightClick ? 1 : _brushes[_selectedBrush].density;
+        float sizeIncrease = rightClick ? 1f : _brushes[_selectedBrush].dispersion;
         HashSet<int> brushTris = new HashSet<int>();
 
         brushTris.Add(SelectTriangle(Input.mousePosition));
@@ -76,7 +81,7 @@ public class PaintingController : MonoBehaviour
         foreach (int t in brushTris)
         {
             if (t < 0) continue;
-            _firstPersonController.currentActiveCanvas.SetTriangleMaterial(t, brushes[_selectedBrush].GetMaterial());
+            _firstPersonController.currentActiveCanvas.SetTriangleMaterial(t, _brushes[_selectedBrush].GetMaterial());
         }
     }
 
@@ -86,7 +91,7 @@ public class PaintingController : MonoBehaviour
         if (!Physics.Raycast(_camera.ScreenPointToRay(originPos), out hit)) return -1;
 
         MeshCollider meshCollider = hit.collider as MeshCollider;
-        if (meshCollider == null || meshCollider.sharedMesh == null) return -1;
+        if (meshCollider == null || meshCollider.sharedMesh == null || meshCollider.gameObject.GetComponent<PaintingCanvas>() == null) return -1;
 
         Mesh mesh = meshCollider.sharedMesh;
         int[] triangles = mesh.triangles;
